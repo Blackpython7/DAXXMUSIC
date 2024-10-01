dbff0dce166cef84bafb379ff13513b75175e246
AVYA <136340837+Blackpython7@users.noreply.github.com>
2024-10-01 23:27:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -69,8 +69,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/gitwizardbypass")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/edition_server_support")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+YE1HGzM8i7QyOGU1")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
